962a2ee4febc7bf95f4d462577992bd80b53afa8
lifetea <494886251@qq.com>
2015-02-07 12:19:04 +0800

    更改Application_image

--- application.php
+++ application.php
@@ -57,96 +57,96 @@
              <p style="margin-top:10px;"><?php echo _('Side channel blowers have been widely used in the environmental water treatment. For example, in the aeration of sewage, ring blowers can provide oxy that the aerobe need in the activated sludge. Also, blowers can provide the conditions which make the sewage and activated sludge mixed sufficiently to dissolve all kinds of organic matters in the water. Then, the sewage water can be purified. Besides the above, ring blowers can also be used to mix the plating bath fluid and can be used in the aquaculture industry as well in the printer industry.');?></p>
              <div class="application_image">
                       <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Punch collector" rel="application_image/punchcollector.swf" class="flash"><img src="application_image/punchcollector.png" alt="side channel blower punch collector application"/></a></dd>
-			          <dd><a href="javascript:;" name="Punch collector" rel="application_image/punchcollector.swf" class="flash" id="align" style="padding-left:55px;"><?php echo _('Punch collector');?></a></dd>
+			          <dd><a href="javascript:;" name="Punch collector" rel="swfs/punchcollector.swf" class="flash"><img src="swfs/punchcollector.png" alt="side channel blower punch collector application"/></a></dd>
+			          <dd><a href="javascript:;" name="Punch collector" rel="swfs/punchcollector.swf" class="flash" id="align" style="padding-left:55px;"><?php echo _('Punch collector');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Sewing machine" rel="application_image/sewingmachine.swf" class="flash"><img src="application_image/sewingmachine.png" alt="side channel blower sewing machine application"/></a></dd>
-			          <dd><a href="javascript:;" name="Sewing machine" rel="application_image/sewingmachine.swf" class="flash" id="align" style="padding-left:50px;"><?php echo _('Sewing machine');?></a></dd>
+			          <dd><a href="javascript:;" name="Sewing machine" rel="swfs/sewingmachine.swf" class="flash"><img src="swfs/sewingmachine.png" alt="side channel blower sewing machine application"/></a></dd>
+			          <dd><a href="javascript:;" name="Sewing machine" rel="swfs/sewingmachine.swf" class="flash" id="align" style="padding-left:50px;"><?php echo _('Sewing machine');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Transfer" rel="application_image/transfer.swf" class="flash"><img src="application_image/transfer.png" alt="side channel blower transfer application"/></a></dd>
-			          <dd><a href="javascript:;" name="Transfer" rel="application_image/transfer.swf" class="flash" id="align" ><?php echo _('Transfer');?></a></dd>
+			          <dd><a href="javascript:;" name="Transfer" rel="swfs/transfer.swf" class="flash"><img src="swfs/transfer.png" alt="side channel blower transfer application"/></a></dd>
+			          <dd><a href="javascript:;" name="Transfer" rel="swfs/transfer.swf" class="flash" id="align" ><?php echo _('Transfer');?></a></dd>
 			          </dl>
 			          <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Paper scrap" rel="application_image/paperscrap.swf" class="flash"><img src="application_image/paperscrap.png" alt="side channel blower paper scrap application"/></a></dd>
-			          <dd><a href="javascript:;" name="Paper scrap" rel="application_image/paperscrap.swf" class="flash" id="align"><?php echo _('Paper scrap');?></a></dd>
+			          <dd><a href="javascript:;" name="Paper scrap" rel="swfs/paperscrap.swf" class="flash"><img src="swfs/paperscrap.png" alt="side channel blower paper scrap application"/></a></dd>
+			          <dd><a href="javascript:;" name="Paper scrap" rel="swfs/paperscrap.swf" class="flash" id="align"><?php echo _('Paper scrap');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Waste gasses" rel="application_image/wastegasses.swf" class="flash"><img src="application_image/wastegasses.png" alt="side channel blower waste gasses application"/></a></dd>
-			          <dd><a href="javascript:;" name="Waste gasses" rel="application_image/wastegasses.swf" class="flash" id="align" ><?php echo _('Waste gasses');?></a></dd>
+			          <dd><a href="javascript:;" name="Waste gasses" rel="swfs/wastegasses.swf" class="flash"><img src="swfs/wastegasses.png" alt="side channel blower waste gasses application"/></a></dd>
+			          <dd><a href="javascript:;" name="Waste gasses" rel="swfs/wastegasses.swf" class="flash" id="align" ><?php echo _('Waste gasses');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Med suction" rel="application_image/medsuction.swf" class="flash"><img src="application_image/medsuction.png" alt="side channel blower med suction application"/></a></dd>
-			          <dd><a href="javascript:;" name="Med suction" rel="application_image/medsuction.swf" class="flash" id="align" ><?php echo _('Med suction');?></a></dd>
+			          <dd><a href="javascript:;" name="Med suction" rel="swfs/medsuction.swf" class="flash"><img src="swfs/medsuction.png" alt="side channel blower med suction application"/></a></dd>
+			          <dd><a href="javascript:;" name="Med suction" rel="swfs/medsuction.swf" class="flash" id="align" ><?php echo _('Med suction');?></a></dd>
 			          </dl>
                       <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Paper dryer" rel="application_image/paperdryer.swf" class="flash"><img src="application_image/paperdryer.png" alt="side channel blower paper dryer application"/></a></dd>
-			          <dd><a href="javascript:;" name="Paper dryer" rel="application_image/paperdryer.swf" class="flash" id="align" ><?php echo _('Paper dryer');?></a></dd>
+			          <dd><a href="javascript:;" name="Paper dryer" rel="swfs/paperdryer.swf" class="flash"><img src="swfs/paperdryer.png" alt="side channel blower paper dryer application"/></a></dd>
+			          <dd><a href="javascript:;" name="Paper dryer" rel="swfs/paperdryer.swf" class="flash" id="align" ><?php echo _('Paper dryer');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Paper feeding machine" rel="application_image/paperfeedingmachine.swf" class="flash"><img src="application_image/paperfeedingmachine.png" alt="side channel blower paper feeding machine application"/></a></dd>
-			          <dd><a href="javascript:;" name="Paper feeding machine" rel="application_image/paperfeedingmachine.swf" class="flash" id="align" style="padding-left:47px;"><?php echo _('Paper feeding machine');?></a></dd>
+			          <dd><a href="javascript:;" name="Paper feeding machine" rel="swfs/paperfeedingmachine.swf" class="flash"><img src="swfs/paperfeedingmachine.png" alt="side channel blower paper feeding machine application"/></a></dd>
+			          <dd><a href="javascript:;" name="Paper feeding machine" rel="swfs/paperfeedingmachine.swf" class="flash" id="align" style="padding-left:47px;"><?php echo _('Paper feeding machine');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Paper holder" rel="application_image/paperholder.swf" class="flash"><img src="application_image/paperholder.png" alt="side channel blower paper holder application"/></a></dd>
-			          <dd><a href="javascript:;" name="Paper holder" rel="application_image/paperholder.swf" class="flash" id="align"><?php echo _('Paper holder');?></a></dd>
+			          <dd><a href="javascript:;" name="Paper holder" rel="swfs/paperholder.swf" class="flash"><img src="swfs/paperholder.png" alt="side channel blower paper holder application"/></a></dd>
+			          <dd><a href="javascript:;" name="Paper holder" rel="swfs/paperholder.swf" class="flash" id="align"><?php echo _('Paper holder');?></a></dd>
 			          </dl>
 			          <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Vacuum transfer" rel="application_image/vacuumtransfer.swf" class="flash"><img src="application_image/vacuumtransfer.png" alt="side channel blower vacuum transfer application"/></a></dd>
-			          <dd><a href="javascript:;" name="Vacuum transfer" rel="application_image/vacuumtransfer.swf" class="flash" id="align" ><?php echo _('Vacuum transfer');?></a></dd>
+			          <dd><a href="javascript:;" name="Vacuum transfer" rel="swfs/vacuumtransfer.swf" class="flash"><img src="swfs/vacuumtransfer.png" alt="side channel blower vacuum transfer application"/></a></dd>
+			          <dd><a href="javascript:;" name="Vacuum transfer" rel="swfs/vacuumtransfer.swf" class="flash" id="align" ><?php echo _('Vacuum transfer');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Powder transfer" rel="application_image/powdertransfer.swf" class="flash"><img src="application_image/powdertransfer.png" alt="side channel blower powder transfer application"/></a></dd>
-			          <dd><a href="javascript:;" name="Powder transfer" rel="application_image/powdertransfer.swf" class="flash" id="align"><?php echo _('Powder transfer');?></a></dd>
+			          <dd><a href="javascript:;" name="Powder transfer" rel="swfs/powdertransfer.swf" class="flash"><img src="swfs/powdertransfer.png" alt="side channel blower powder transfer application"/></a></dd>
+			          <dd><a href="javascript:;" name="Powder transfer" rel="swfs/powdertransfer.swf" class="flash" id="align"><?php echo _('Powder transfer');?></a></dd>
 			          </dl>
                       <dl>
-			          <dd><a href="javascript:;" name="Collector" rel="application_image/collector.swf" class="flash"><img src="application_image/collector.png" alt="side channel blower Collector application"/></a></dd>
-			          <dd><a href="javascript:;" name="Collector" rel="application_image/collector.swf" class="flash" id="align"><?php echo _('Collector');?></a></dd>
+			          <dd><a href="javascript:;" name="Collector" rel="swfs/collector.swf" class="flash"><img src="swfs/collector.png" alt="side channel blower Collector application"/></a></dd>
+			          <dd><a href="javascript:;" name="Collector" rel="swfs/collector.swf" class="flash" id="align"><?php echo _('Collector');?></a></dd>
 			          </dl>
              		  <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Sediment" rel="application_image/sediment2.swf" class="flash"><img src="application_image/sediment2.png" alt="side channel blower Sediment application"/></a></dd>
-			          <dd><a href="javascript:;" name="Sediment" rel="application_image/sediment2.swf" class="flash" id="align"><?php echo _('Sediment');?></a></dd>
+			          <dd><a href="javascript:;" name="Sediment" rel="swfs/sediment2.swf" class="flash"><img src="swfs/sediment2.png" alt="side channel blower Sediment application"/></a></dd>
+			          <dd><a href="javascript:;" name="Sediment" rel="swfs/sediment2.swf" class="flash" id="align"><?php echo _('Sediment');?></a></dd>
 			          </dl>
              		  <dl>
-			          <dd><a href="javascript:;" name="Ventilation" rel="application_image/tunnels.swf" class="flash"><img src="application_image/tunnels.png" alt="side channel blower Ventilation application"/></a></dd>
-			          <dd><a href="javascript:;" name="Ventilation" rel="application_image/tunnels.swf" class="flash" id="align"><?php echo _('Ventilation');?></a></dd>
+			          <dd><a href="javascript:;" name="Ventilation" rel="swfs/tunnels.swf" class="flash"><img src="swfs/tunnels.png" alt="side channel blower Ventilation application"/></a></dd>
+			          <dd><a href="javascript:;" name="Ventilation" rel="swfs/tunnels.swf" class="flash" id="align"><?php echo _('Ventilation');?></a></dd>
 			          </dl>
 			          <dl>
-			          <dd><a href="javascript:;" name="Hopper Loader" rel="application_image/hopper.swf" class="flash"><img src="application_image/hopper.png" alt="side channel blower Hopper Loader application"/></a></dd>
-			          <dd><a href="javascript:;" name="Hopper Loader" rel="application_image/hopper.swf" class="flash" id="align"><?php echo _('Hopper Loader');?></a></dd>
+			          <dd><a href="javascript:;" name="Hopper Loader" rel="swfs/hopper.swf" class="flash"><img src="swfs/hopper.png" alt="side channel blower Hopper Loader application"/></a></dd>
+			          <dd><a href="javascript:;" name="Hopper Loader" rel="swfs/hopper.swf" class="flash" id="align"><?php echo _('Hopper Loader');?></a></dd>
 			          </dl>
 			          <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Textile" rel="application_image/autoloom.swf" class="flash"><img src="application_image/autoloom.png" alt="side channel blower Textile application"/></a></dd>
-			          <dd><a href="javascript:;" name="Textile" rel="application_image/autoloom.swf" class="flash" id="align"><?php echo _('Textile');?></a></dd>
+			          <dd><a href="javascript:;" name="Textile" rel="swfs/autoloom.swf" class="flash"><img src="swfs/autoloom.png" alt="side channel blower Textile application"/></a></dd>
+			          <dd><a href="javascript:;" name="Textile" rel="swfs/autoloom.swf" class="flash" id="align"><?php echo _('Textile');?></a></dd>
 			          </dl>
 			          <dl>
-			          <dd><a href="javascript:;" name="Bottling" rel="application_image/bottling.swf" class="flash"><img src="application_image/bottling.png" alt="side channel blower Bottling application"/></a></dd>
-			          <dd><a href="javascript:;" name="Bottling" rel="application_image/bottling.swf" class="flash" id="align"><?php echo _('Bottling');?></a></dd>
+			          <dd><a href="javascript:;" name="Bottling" rel="swfs/bottling.swf" class="flash"><img src="swfs/bottling.png" alt="side channel blower Bottling application"/></a></dd>
+			          <dd><a href="javascript:;" name="Bottling" rel="swfs/bottling.swf" class="flash" id="align"><?php echo _('Bottling');?></a></dd>
 			          </dl>
 			          <dl>
-			          <dd><a href="javascript:;" name="Electro facing" rel="application_image/circulator.swf" class="flash"><img src="application_image/circulator.png" alt="side channel blower Electro facing application"/></a></dd>
-			          <dd><a href="javascript:;" name="Electro facing" rel="application_image/circulator.swf" class="flash" id="align"><?php echo _('Electro facing');?></a></dd>
+			          <dd><a href="javascript:;" name="Electro facing" rel="swfs/circulator.swf" class="flash"><img src="swfs/circulator.png" alt="side channel blower Electro facing application"/></a></dd>
+			          <dd><a href="javascript:;" name="Electro facing" rel="swfs/circulator.swf" class="flash" id="align"><?php echo _('Electro facing');?></a></dd>
 			          </dl>
 			          <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Dryer" rel="application_image/dryer.swf" class="flash"><img src="application_image/dryer.png" alt="side channel blower Dryer application"/></a></dd>
-			          <dd><a href="javascript:;" name="Dryer" rel="application_image/dryer.swf" class="flash" id="align"><?php echo _('Dryer');?></a></dd>
+			          <dd><a href="javascript:;" name="Dryer" rel="swfs/dryer.swf" class="flash"><img src="swfs/dryer.png" alt="side channel blower Dryer application"/></a></dd>
+			          <dd><a href="javascript:;" name="Dryer" rel="swfs/dryer.swf" class="flash" id="align"><?php echo _('Dryer');?></a></dd>
 			          </dl>
 			          <dl>
-			          <dd><a href="javascript:;" name="Eliminator" rel="application_image/eliminator.swf" class="flash"><img src="application_image/eliminator.png" alt="side channel blower Eliminator application"/></a></dd>
-			          <dd><a href="javascript:;" name="Eliminator" rel="application_image/eliminator.swf" class="flash" id="align"><?php echo _('Eliminator');?></a></dd>
+			          <dd><a href="javascript:;" name="Eliminator" rel="swfs/eliminator.swf" class="flash"><img src="swfs/eliminator.png" alt="side channel blower Eliminator application"/></a></dd>
+			          <dd><a href="javascript:;" name="Eliminator" rel="swfs/eliminator.swf" class="flash" id="align"><?php echo _('Eliminator');?></a></dd>
 			          </dl>
 			          <dl>
-			          <dd><a href="javascript:;" name="Evaporator" rel="application_image/evaporator.swf" class="flash"><img src="application_image/evaporator.png" alt="side channel blower Evaporator application"/></a></dd>
-			          <dd><a href="javascript:;" name="Evaporator" rel="application_image/evaporator.swf" class="flash" id="align"><?php echo _('Evaporator');?></a></dd>
+			          <dd><a href="javascript:;" name="Evaporator" rel="swfs/evaporator.swf" class="flash"><img src="swfs/evaporator.png" alt="side channel blower Evaporator application"/></a></dd>
+			          <dd><a href="javascript:;" name="Evaporator" rel="swfs/evaporator.swf" class="flash" id="align"><?php echo _('Evaporator');?></a></dd>
 			          </dl>
 			          <dl style="margin-left:-18px;">
-			          <dd><a href="javascript:;" name="Sock Knitting" rel="application_image/sockloom.swf" class="flash"><img src="application_image/sockloom.png" alt="side channel blower Sock Knitting application"/></a></dd>
-			          <dd><a href="javascript:;" name="Sock Knitting" rel="application_image/sockloom.swf" class="flash" id="align"><?php echo _('Sock Knitting');?></a></dd>
+			          <dd><a href="javascript:;" name="Sock Knitting" rel="swfs/sockloom.swf" class="flash"><img src="swfs/sockloom.png" alt="side channel blower Sock Knitting application"/></a></dd>
+			          <dd><a href="javascript:;" name="Sock Knitting" rel="swfs/sockloom.swf" class="flash" id="align"><?php echo _('Sock Knitting');?></a></dd>
 			          </dl>
 			          <dl>
-			          <dd><a href="javascript:;" name="Pool & Psa" rel="application_image/jacuzzi.swf" class="flash"><img src="application_image/jacuzzi.png" alt="side channel blower Pool & Psa application"/></a></dd>
-			          <dd><a href="javascript:;" name="Pool & Psa" rel="application_image/jacuzzi.swf" class="flash" id="align"><?php echo _('Pool & Psa');?></a></dd>
+			          <dd><a href="javascript:;" name="Pool & Psa" rel="swfs/jacuzzi.swf" class="flash"><img src="swfs/jacuzzi.png" alt="side channel blower Pool & Psa application"/></a></dd>
+			          <dd><a href="javascript:;" name="Pool & Psa" rel="swfs/jacuzzi.swf" class="flash" id="align"><?php echo _('Pool & Psa');?></a></dd>
 			         </dl>
              </div>
        </div>
