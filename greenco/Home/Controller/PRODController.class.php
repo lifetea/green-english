4aa67a581b5e648c8c4e335b751746088a023bd4
lifetea <494886251@qq.com>
2015-05-25 17:30:18 +0800

    fixs #2 优化搜索

--- greenco/Home/Controller/PRODController.class.php
+++ greenco/Home/Controller/PRODController.class.php
@@ -21,33 +21,35 @@ class PRODController extends Controller {
     	$prod = M("product");
     	$airFlow = I('get.airFlow');
     	$pressure = I('get.pressure');
+    	$type = I('get.type');
+		if($type=="maximum_vacuum"){
+			$pressure = -$pressure;
+		}    	
     	if(empty($airFlow) && empty($pressure)){
     		E('请添加搜索条件');
     		return false;
+    	}elseif (empty($airFlow) && !empty($pressure)){
+    		$sql =$type."=".$pressure." ";
+    	}elseif (!empty($airFlow) && empty($pressure)){
+    		$sql ="maximum_airflow=".$airFlow." " ; 
+    	}else{
+    		$sql ="maximum_airflow"."=".$airFlow." and ".$type."=".$pressure." " ;
     	}
-    	$sql =!empty($airFlow) ? "maximum_airflow =".$airFlow." " : "maximum_pressure=".$pressure." ";
-    	$sql =!empty($pressure) && !empty($airFlow) ? "maximum_airflow=".$airFlow." and maximum_pressure=".$pressure." " :"";
     	$list = $prod->where($sql)->select();
-    	//S('contact-data', $list, 7000);
     	$this->assign('series',I('get.series'));
     	$this->assign('list',$list);
     	$this->display();
     }    
     
     public function Content(){
-    	//product  type like '%2RB {$_GET['series']}%'
-    	//$list = S('contact-data');
-    	//if (!$list) {
-   //  like '%{$_GET['series']}%' and output={$_GET['output']}
-    			
     	$prod = M("product");
     	$list = $prod->where("type like '%{$_GET['series']}%' and output={$_GET['output']}")->find();
-    	//S('contact-data', $list, 7000);
-    	//}
     	$this->assign('series',I('get.series'));
     	$list["subtype"] = substr($list["type"],0,7);
+    	if($list["subtype"] =="3RB 350"){
+    		$list["subtype"] = substr($list["type"],0,9);
+    	}
     	$this->assign('list',$list);
-    	//dump($list);
     	$this->display();
     }
     public function IEContent(){
