dd3228d17c21cca52adde5d0a0aa46dc6bc6ab89
lifetea <494886251@qq.com>
2015-06-02 08:18:30 +0800

    fixs #1

--- test.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <script type="text/javascript"  data-main="src/js/main" src="src/js/require.js"></script>
-    </head>
-    <body>
-      <span id="test">body</span>
-    </body>
-</html>
