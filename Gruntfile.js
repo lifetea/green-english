574821ab7906da8f57466da4e428411f67cf8bff
lifetea <494886251@qq.com>
2015-06-04 11:13:58 +0800

    fixs #6 添加ImageMin

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,29 @@ module.exports = function(grunt) {
           'src/js/lib/search.min.js': ['src/js/lib/search.js']
         }
       }
+    },
+    imagemin: {
+      /* 压缩图片大小 */
+      dist: {
+          options: {
+              optimizationLevel: 3 //定义 PNG 图片优化水平
+          },
+          files: [
+              {
+              expand: true,
+              cwd: 'src/img/',
+              src: ['**/*.{png,jpg,jpeg}'], // 优化 img 目录下所有 png/jpg/jpeg 图片
+              dest: 'src/img1/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+              }
+            ]
+          }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['uglify','imagemin']);
 
 };
