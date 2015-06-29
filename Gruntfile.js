09248eec10081a97481622d68f80cfef238e8b8a
lifetea <494886251@qq.com>
2015-06-29 17:05:49 +0800

    fixs #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,6 @@ module.exports = function(grunt) {
       },
       my_target: {
         files: {
-          'src/js/lib/swfobject.min.js': ['src/js/lib/swfobject.js'],
           'src/js/lib/search.min.js': ['src/js/lib/search.js']
         }
       }
@@ -23,19 +22,35 @@ module.exports = function(grunt) {
           files: [
               {
               expand: true,
-              cwd: 'src/img/',
+              cwd: 'src/en/',
               src: ['**/*.{png,jpg,jpeg}'], // 优化 img 目录下所有 png/jpg/jpeg 图片
-              dest: 'src/img1/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+              dest: 'src/en/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
               }
             ]
           }
-    }
+    },
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: [{
+              expand: true,
+              cwd: 'src/css/',
+              src: ['*.css', '!*.min.css'],
+              dest: 'src/css/',
+              ext: '.min.css'
+            }]
+      }
+    }    
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   // Default task(s).
-  grunt.registerTask('default', ['uglify','imagemin']);
+  grunt.registerTask('default', ['uglify','imagemin','cssmin']);
 
 };
